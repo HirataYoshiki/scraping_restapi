8353cbd54c58041d1c6a5623b210f2ab2e59bb24
Yoshiki Hirata <kirryx2@gmail.com>
2021-01-31 10:55:58 +0900

    make .gitignore, add pipfile,pipfile.loc

--- scraping/controls/Control.py
+++ scraping/controls/Control.py
@@ -1,5 +1,18 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-def get_results(url:str,*args):
-    return "this is test"
+def get_results(url:str,*tags,methods = "or"):
+    if url = "" or tags = "":
+        if url = "":
+            text = "please enter the url.\n"
+        if tag = "":
+            text_2 = "please enter the tags."    
+            text += text_2
+        return text
+    
+    res = requests.get(url)
+    soup = bs(res.text,"html.parser")
+
+
+
+
